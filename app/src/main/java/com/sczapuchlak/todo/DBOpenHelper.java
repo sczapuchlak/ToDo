64ee6ce065ddee1ba214496d06b2d3e0f608d7bf
stephanie zap <iamstephanie@gmail.com>
2017-12-07 00:20:05 -0600

    new

--- app/src/main/java/com/sczapuchlak/todo/DBOpenHelper.java
+++ app/src/main/java/com/sczapuchlak/todo/DBOpenHelper.java
@@ -10,25 +10,28 @@ import android.database.sqlite.SQLiteOpenHelper;
 import java.util.ArrayList;
 
 public class DBOpenHelper extends SQLiteOpenHelper{
-
+    private static final String DATABASE_NAME = "toDo.db";
+    private static final String TABLE_NAME = "toDo";
+    private static final String COL_NAME = "items";
 
     DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, DATABASE_NAME, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sql = "CREATE TABLE ToDo (ToDo Items)";
+        String sql = "CREATE TABLE"+ TABLE_NAME;
         sqLiteDatabase.execSQL(sql);
+//        sqLiteDatabase.delete("ToDo",null,null );
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("drop table if exists ToDo");
-    }
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+}
 
     void add(String item){
-        String sql = "insert into table ToDo (Items) values (" + item+ ")";
+        String sql = "insert into table"+TABLE_NAME+ "(Items) values (" + item+ ")";
 
         ContentValues values = new ContentValues();
         values.put("Items", item);
@@ -37,7 +40,7 @@ public class DBOpenHelper extends SQLiteOpenHelper{
         db.close();
     }
     ArrayList<String> getAll(){
-        String sql = "select * from ToDo";
+        String sql = "select * from"+ TABLE_NAME;
         SQLiteDatabase db = this. getWritableDatabase();
         Cursor cursor = db.rawQuery(sql, null);
 
